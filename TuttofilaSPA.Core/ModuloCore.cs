5c2cf2a7acf391b7fe32d4434b4db65137a7ecac
Mirko Maggioni <mirko.maggioni@gmail.com>
2019-02-08 22:23:18 +0100

    Adding autofac module

---
+++ TuttofilaSPA.Core/ModuloCore.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace TuttofilaSPA.Core
+{
+	public class ModuloCore : Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder.RegisterType<ContextFactory>().AsSelf().SingleInstance();
+		}
+	}
+}
